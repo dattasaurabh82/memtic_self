7424dd717638dcc2d9550509a68f11d00374934f
Saurabh Datta <hi@dattasaurabh.com>
2022-05-09 19:57:34 +0800

    removed tesrt.py

--- processing_part_nucV2/test.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logging.basicConfig(level=logging.DEBUG)
-
-logging.debug('This is a debug message')
-logging.info('This is an info message')
-logging.warning('This is a warning message')
-logging.error('This is an error message')
-logging.critical('This is a critical message')
